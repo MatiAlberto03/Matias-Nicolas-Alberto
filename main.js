c47ff090d12000147bb6b1465f2a0b1fd8b78508
MatiAlberto03 <malberto@itba.edu.ar>
2022-05-25 20:44:48 -0300

    PW

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-function borrarPais(){
-    let paises= document.getElementsByClassName("paises")
-    paises[0].remove()
-}
 
-function sumarEdad(){
-    let nombreUsuario = document.getElementById("nombre").value
-    let edadUsuario = document.getElementById("edad").value
-    document.body.append("El nombre del usuario es: " + nombreUsuario + "y va a cumplir " + (edadUsuario+1))
-
-
-}
