1c53c50849316724cc15fd04d36bbd9f02cc39c7
MatiAlberto03 <malberto@itba.edu.ar>
2022-05-25 21:08:07 -0300

    pw

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+function ConfirmDelete()
+{
+    let respuesta = confirm("Estas seguro que desea cerrar la pagina?")
+    if (respuesta == tue)
+    {
+        return true;
+    }
+    else 
+    {
+        return false
+    }
+}
+
+
 
