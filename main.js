40414670acf26a58c36008f2a349e468b9b738cd
MatiAlberto03 <malberto@itba.edu.ar>
2022-05-27 17:29:52 -0300

    pw

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
-function ConfirmDelete()
-{
-    let respuesta = confirm("Estas seguro que desea cerrar la pagina?")
-    if (respuesta == tue)
-    {
-        return true;
+let horas = 7;
+let minutos = 0;
+let segundos = 0;
+
+
+function cargarSegundo(){
+    let txtSegundos;
+    
+    
+    if(segundos < 0){
+        segundos = 59; 
     }
-    else 
-    {
-        return false
+    else if(segundos < 10){
+        txtSegundos = `${segundos}`;
+    }else{
+        txtSegundos = segundos;
+
     }
+    const $seleccion = document.getElementById("segundos"); 
+    $seleccion.innerHTML = txtSegundos
+    segundos --;
 }
 
+cargarSegundo();
 
 
+setInterval(cargarSegundo, 1000);
+
+function cargarMinutos(segundos){
+    txtMinutos;
+}
