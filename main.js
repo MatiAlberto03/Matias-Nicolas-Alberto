2335f3058d790cc4c62c5222dbea5eae899c6f4a
MatiAlberto03 <malberto@itba.edu.ar>
2022-05-27 19:28:47 -0300

    pw

--- main.js
+++ main.js
@@ -1,31 +1,45 @@
 let horas = 7;
 let minutos = 0;
 let segundos = 0;
-
-
-function cargarSegundo(){
-    let txtSegundos;
-    
-    
-    if(segundos < 0){
-        segundos = 59; 
-    }
-    else if(segundos < 10){
-        txtSegundos = `${segundos}`;
-    }else{
-        txtSegundos = segundos;
-
-    }
-    const $seleccion = document.getElementById("segundos"); 
-    $seleccion.innerHTML = txtSegundos
-    segundos --;
-}
-
 cargarSegundo();
 
 
-setInterval(cargarSegundo, 1000);
+ function cargarSegundo(){
+     let txtSegundos;
 
-function cargarMinutos(segundos){
+     if(segundos < 0){
+         segundos = 59;
+     }
+
+     if(segundos < 10){
+         txtSegundos = `${segundos}`;
+     }else{
+         txtSegundos = segundos;
+
+     }
+     document.getElementById("segundos").innerHTML = txtSegundos;
+     segundos --;   
+}
+     
+
+ function cargarMinutos(segundos){
     txtMinutos;
+
+   if(segundos == -1 && minutos !==0){
+       setTimeout(() =>{
+           minutos--;
+       },500)
+
+   }else if(segundos == -1 && minutos !==0){
+       setTimeout(() => {
+           minutos = 59;
+       },500)
+   }
+
+   if(minutos < 10){
+        txtMinutos = `${minutos}`;
+    }else{
+         txtMinutos = minutos;
+    }   
+    document.getElementById("minutos").innerHTML = txtMinutos;
 }
